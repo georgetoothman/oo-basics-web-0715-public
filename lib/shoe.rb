ae2c81aec0357e62654fa67d9335c05b09128c32
George Toothman <george.toothman@gmail.com>
2015-08-05 13:05:28 -0400

    create classes for shoe and book

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+# Make your shoe class here
+class Shoe
+  attr_accessor :color, :size, :brand, :material, :condition
+  attr_reader 
+  
+  BRANDS = []
+  
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
